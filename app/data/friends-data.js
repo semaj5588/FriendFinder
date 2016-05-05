9d38e1cc8d87ac799d5581a34ab297304d0ad2d9
James <semaj5588@gmail.com>
2016-05-05 18:21:21 -0400

    added hw

---
+++ app/data/friends-data.js
@@ -0,0 +1,21 @@
+var friendArray = [
+	{
+		name: "James",
+		photo: "",
+		scores: [
+		5,
+		1,
+		4,
+		4,
+		5,
+		1,
+		2,
+		5,
+		4,
+		1
+		]
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require. 
+module.exports = friendArray; 
---
+++ app/routing/api-routes.js
@@ -0,0 +1,44 @@
+var friendsData 	= require('../data/friends-data.js');
+var path 			= require('path');
+
+
+
+module.exports = function(app){
+
+	// API GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases when a user visits a link 
+	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
+	// ---------------------------------------------------------------------------
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendsData);
+	});
+
+
+	// API POST Requests
+	// Below code handles when a user submits a form and thus submits data to the server.
+	// In each of the below cases, when a user submits form data (a JSON object)
+	// ...the JSON is pushed to the appropriate Javascript array
+	// (ex. User fills out a reservation request... this data is then sent to the server...
+	// Then the server saves the data to the tableData array)
+	// ---------------------------------------------------------------------------
+
+
+	app.post('/api/friends-data', function(req, res){
+
+		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+		// It will do this by sending out the value "true" have a table 
+		if(friends.length < 5 ){
+			friends.push(req.body);
+			res.json(true); // KEY LINE
+		}
+
+		// Or false if they don't have a table
+		else{
+			waitListData.push(req.body);
+			res.json(false); // KEY LINE
+		}
+
+	});
+}
